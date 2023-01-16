6412ff0ce5ab60c029e757373ef409f8e8778155
cwen-booth <cwen@chicagobooth.edu>
2023-01-16 06:12:26 +0000

    integer hw

--- integer_birth_year.rb
+++ integer_birth_year.rb
@@ -1,9 +1,11 @@
 # Ask for a person’s age, and then calculate and display what year they were born in.
-# 
+#
 # Input:
 #   80
-# 
+#
 # Key output:
 #   "Wow, you were born in 1940. You're old!"
 
 p "How old are you?"
+age = gets.to_i
+p "Wow, you were born in #{2023 - age}. You're old!"
--- integer_odd.rb
+++ integer_odd.rb
@@ -1,4 +1,4 @@
-# should ask the user to enter a number and output 'true' or 'false' 
+# should ask the user to enter a number and output 'true' or 'false'
 # and if the number is odd
 # Example:
 #   "Enter an odd number:"
@@ -10,3 +10,5 @@
 #   false
 
 p "Enter an odd number:"
+input = gets.to_i
+p input.odd?
